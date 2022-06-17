0c0bea825dc18d762fe9519313920125963ea4fe
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-06-17 23:25:58 +0300

    initial commit
    initial commit

---
+++ src/main/java/com/gabrlaur/idvalidationdemo/IdValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrlaur.idvalidationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdValidationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdValidationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrlaur/idvalidationdemo/IdValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrlaur.idvalidationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
